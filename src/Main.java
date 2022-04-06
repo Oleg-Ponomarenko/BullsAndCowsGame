ff5d847674b42d2f6d5b3ba379e432386924fb6d
ponomarenkoog.22 <ponomarenko1711@mail.ru>
2022-04-06 12:58:31 +0300

    "Хитрость" недоработана

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,28 @@ public class Main {
         int number;
         final Random rand = new Random();
         number = rand.nextInt(8999) + 1000;
-        System.out.println("I generated a four-digit number. Try to guess it.");
+        System.out.println("I generated a four-digit number. Try to guess it." + number);
+
+        //Массив для хранения недоопределённых чисел
+        boolean[][] unused;
+        unused = new boolean[4][10];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 10; j ++) {
+                unused[i][j] = true;
+            }
+        }
 
         boolean rightAns = false;
+        boolean otherNums = true;
         Scanner in = new Scanner(System.in);
-        while (!rightAns) {
+        while (!rightAns && otherNums) {
             int answer = in.nextInt();
+
+            unused[0][answer / 1000] = false;
+            unused[1][(answer / 100) % 10] = false;
+            unused[2][(answer / 10) % 10] = false;
+            unused[3][answer % 10] = false;
+
             byte bulls = 0;
 
             if (answer % 10 == number % 10) {
@@ -32,6 +48,20 @@ public class Main {
             }
             else {
                 System.out.println(bulls + " bulls and " + (4 - bulls) + " cows");
+            }
+
+            if (rightAns) {
+                otherNums = false;
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 10; j ++) {
+                        if (unused[i][j]) {
+                            otherNums = true;
+                            number = (number - (number / (int)Math.pow(10, (3 - i))) % (int)Math.pow(10, i)) + j * (int)Math.pow(10, (3 - i));
+                            rightAns = false;
+                            System.out.println("Number changed: " + number);
+                        }
+                    }
+                }
             }
         }
         System.out.println("4 bulls. The number was " + number);
